a7e6af8ab419a2d8b23303f0fccd9ab2cd6fe420
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-05-29 16:25:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ st.markdown(
 
     .header-with-bg {
         background-image: url('https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/header_bg.png');
-        width: 300px;
-        height: 300px;
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center;
