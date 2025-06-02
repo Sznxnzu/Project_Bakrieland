415d81875bef64761bdff36218678cf90db678a7
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-03 06:42:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import google.generativeai as genai
+from PIL import Image
+import io
 
 st.set_page_config(layout="wide", page_title="Bakrieland Mood Analytic", initial_sidebar_state="collapsed")
 
