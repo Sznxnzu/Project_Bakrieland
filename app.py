0b8f338871a50a8438f38bbe0a0ed40d555f77fd
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-02 13:48:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ html, body, [data-testid="stAppViewContainer"] {
 }
 div[data-testid="stCameraInput"] > div {
     aspect-ratio: 4 / 5;
-    width: 40% !important;
+    width: 60% !important;
     height: auto !important;
     margin: 0;
     border-radius: 20px;
