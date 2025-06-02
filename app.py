bbc0a6c1023d1cb2d1a0326ef35190b440501151
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-02 12:32:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,27 +55,52 @@ with col_header_right:
     <img src='https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/metrodata_logo.png' width='50'>
     </p>
     """, unsafe_allow_html=True)
+  components.html(
+    """
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+
+    <model-viewer id="robot" 
+        src="https://cdn.jsdelivr.net/gh/husnanali05/FP_Datmin@main/nerinho_-_mascote_da_neomind.glb"
+        alt="Robot Nerinho 3D"
+        camera-controls 
+        auto-rotate 
+        autoplay
+        style="width: 100%; height: 400px;"
+        ar 
+        shadow-intensity="1"
+        environment-image="neutral"
+        exposure="1"
+        interaction-prompt="none">
+    </model-viewer>
+
+    <script>
+      const robot = document.querySelector("#robot");
+      robot.addEventListener("click", () => {
+        robot.currentTime = 0;
+        robot.play();
+      });
+    </script>
+    """,
+    height=420
+  )
 
 with col_header_left:
   col1, col2, col3 = st.columns([1, 1, 1.4])
+  with col1:
+      st.markdown('<div class="header-box">PROPERTY RECOMMENDATION</div>', unsafe_allow_html=True)
+      st.markdown('<div class="portrait-box">', unsafe_allow_html=True)
+      st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/property_image.jpeg", caption="KAHURIPAN NIRWANA", use_container_width=True)
+      st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/property_image.jpeg", caption="BOGOR NIRWANA RESIDENCE", use_container_width=True)
+      st.markdown('</div>', unsafe_allow_html=True)
 
-with col1:
-    st.markdown('<div class="header-box">PROPERTY RECOMMENDATION</div>', unsafe_allow_html=True)
-    st.markdown('<div class="portrait-box">', unsafe_allow_html=True)
-    st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/property_image.jpeg", caption="KAHURIPAN NIRWANA", use_container_width=True)
-    st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/property_image.jpeg", caption="BOGOR NIRWANA RESIDENCE", use_container_width=True)
-    st.markdown('</div>', unsafe_allow_html=True)
+  with col2:
+      st.markdown('<div class="header-box">HOLIDAY RECOMMENDATION</div>', unsafe_allow_html=True)
+      st.markdown('<div class="portrait-box">', unsafe_allow_html=True)
+      st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/themepark_image.jpg", caption="JUNGLE SEA", use_container_width=True)
+      st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/themepark_image.jpg", caption="RIVERA OUTBOND", use_container_width=True)
+      st.markdown('</div>', unsafe_allow_html=True)
 
-with col2:
-    st.markdown('<div class="header-box">HOLIDAY RECOMMENDATION</div>', unsafe_allow_html=True)
-    st.markdown('<div class="portrait-box">', unsafe_allow_html=True)
-    st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/themepark_image.jpg", caption="JUNGLE SEA", use_container_width=True)
-    st.image("https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/themepark_image.jpg", caption="RIVERA OUTBOND", use_container_width=True)
-    st.markdown('</div>', unsafe_allow_html=True)
-
-with col3:
-    cam_col, robot_col = st.columns([1.2, 1])
-    with cam_col:
+  with col3:
         picture = st.camera_input("Ambil foto wajah Anda", label_visibility="collapsed")
         if picture:
             st.image(picture, width=200)
@@ -101,33 +126,3 @@ with col3:
                 </ul>
             </div>
             """, unsafe_allow_html=True)
-
-    with robot_col:
-        components.html(
-            """
-            <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-
-            <model-viewer id="robot" 
-                src="https://cdn.jsdelivr.net/gh/husnanali05/FP_Datmin@main/nerinho_-_mascote_da_neomind.glb"
-                alt="Robot Nerinho 3D"
-                camera-controls 
-                auto-rotate 
-                autoplay
-                style="width: 100%; height: 400px;"
-                ar 
-                shadow-intensity="1"
-                environment-image="neutral"
-                exposure="1"
-                interaction-prompt="none">
-            </model-viewer>
-
-            <script>
-              const robot = document.querySelector("#robot");
-              robot.addEventListener("click", () => {
-                robot.currentTime = 0;
-                robot.play();
-              });
-            </script>
-            """,
-            height=420
-        )
