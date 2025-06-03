1c84bb258c991b7a3b6d96aa448b3fed41850cdd
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-04 03:55:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -284,6 +284,8 @@ with col_header_left:
                 st.session_state.image_states = updated_image_urls
                 st.session_state.image_captions = updated_image_captions
                 st.session_state.image_analysis = [updated_image_analysis]
+                st.session_state.process_triggered = False 
+                st.rerun()
 
               if st.button("Process Photo"):
                 st.session_state.allow_display = True
