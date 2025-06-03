ed8bbd8324bb086926eef827513ccb114e7ac45b
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-03 11:03:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import requests
 
 # Initialize the image URL in session state
 if "image_url" not in st.session_state:
-    st.session_state.image_url = "https://via.placeholder.com/300?text=Initial+Image"
+    st.session_state.image_url = "https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/qr_logo.png"
 
 def update_image():
     # Update the image URL to a new one or generate a new image
-    st.session_state.image_url = "https://via.placeholder.com/300?text=Updated+Image"
+    st.session_state.image_url = "https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/metrodata_logo.png"
 
 st.image(st.session_state.image_url)
 
