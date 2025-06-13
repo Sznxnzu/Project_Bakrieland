224e6d62402be962ebcedcd845c13c55aa3e002e
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-13 16:58:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,15 @@ div[data-testid="stCameraInputWebcamStyledBox"] {
   box-shadow: 0 0 20px rgba(0,240,255,0.5);
 }
 
+div[data-testid="stCameraInput"] > div {
+    border-radius: 50% !important;
+    width: 250px !important;      
+    height: 250px !important;     
+    margin: 0 auto !important;        
+    box-shadow: 0 0 20px rgba(0,240,255,0.5);
+    transition: transform 0.3s ease;
+}
+
 div[data-testid="stCameraInput"] video,
 div[data-testid="stCameraInput"] img {
     object-fit: cover;
