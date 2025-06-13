6ee90d3f36301b3214cfad45c5eb34485becd30d
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-13 16:21:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ div[data-testid="stCameraInput"] > div {
     width: 300px !important;
     height: 300px !important;
     border-radius: 50% !important;
-    overflow: hidden;
+    overflow: show;
     position: relative;
     box-shadow: 0 0 20px rgba(0,240,255,0.5);
     margin-bottom: 10px;
