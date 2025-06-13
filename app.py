b79574668d02eed687c15ccce8a9073da8b41edc
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-13 15:07:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,8 @@ with row2:
     escaped_analysis = html.escape(st.session_state.analysis_result)
     st.markdown(f"""
     <div class="mood-box-content">
-      <pre style="white-space: pre-wrap; font-family: inherit; font-size: 36px;">{escaped_analysis}</pre>
+      <p>Mood Analytic</p>
+      <pre style="white-space: pre-wrap; font-family: inherit;">{escaped_analysis}</pre>
     </div>
     """, unsafe_allow_html=True)
 
