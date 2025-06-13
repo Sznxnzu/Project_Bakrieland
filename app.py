c7f63527730d4a7de5d19442b98c94e93858da3a
Kevin Alexander Andreas <152668801+Sznxnzu@users.noreply.github.com>
2025-06-13 12:05:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -178,18 +178,17 @@ with row1:
       colA3row11 = st.container()
       with colA3row11:
         st.markdown("""
-        <div style="position: absolute; top: -30px; right: -70px;">
+        <div>
             <img src="https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/resources/logo/bakrieland_logo.png" style="height: 70px; margin-bottom: 4px;" />
-            <div style="font-size: 11px; color: #ccc;">
+        </div>
         """, unsafe_allow_html=True)
       colA3row12 = st.container()
       with colA3row12:
         st.markdown("""
-        <div style="position: absolute; top: -30px; right: -70px;">
+        <div">
                 <span style="display: inline-block; vertical-align: middle;">POWERED BY:</span>
                 <img src="https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/resources/logo/google_logo.png" style="height: 40px; vertical-align: middle; margin-left: -10px; margin-right: -30px;" />
                 <img src="https://raw.githubusercontent.com/Sznxnzu/Project_Bakrieland/main/resources/logo/metrodata_logo.png" style="height: 40px; vertical-align: middle;" />
-            </div>
         </div>
         """, unsafe_allow_html=True)
 
