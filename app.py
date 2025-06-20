f86807398929fcf2644ad7c54f77be6a28af50d4
husnanali05 <168099948+husnanali05@users.noreply.github.com>
2025-06-20 09:40:45 +0000

    test

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ html, body, [data-testid="stAppViewContainer"], .stApp {
   width: 750px;
   height: 750px;
   top: -110px;
-  left: 10;
+  left: -10;
   background-image: url("https://raw.githubusercontent.com/husnanali05/FP_Datmin/main/Halaman%20Story%20WA%20(1).png");
   background-size: contain;
   background-repeat: no-repeat;
